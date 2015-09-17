349226d2123f66cf4c88726ca8ddd1ad008342d5
Peter Tackage <peter.tackage@futurice.com>
2015-09-17 17:14:05 +0300

    Neaten

--- app/src/main/java/com/moac/android/sampleapplication/SampleActivity.java
+++ app/src/main/java/com/moac/android/sampleapplication/SampleActivity.java
@@ -14,13 +14,18 @@ public class SampleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the Activity's UI from XML layout .
         setContentView(R.layout.activity_sample);
 
+        // Get a reference to the Button.
         final Button feedMeButton = (Button) findViewById(R.id.button_feedMe);
 
+        // Add an action to be performed when the button is clicked.
         feedMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create and show a Snackbar UI component on the UI.
                 final Snackbar snackbar = Snackbar.make(feedMeButton, "I'm a Snackbar!", Snackbar.LENGTH_LONG);
                 snackbar.setAction(android.R.string.ok, new View.OnClickListener() {
                     @Override
